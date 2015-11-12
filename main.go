178fb88128f5ba61b062c502d98917f5e048aabd
Steven Wolfe <steven@swolfe.me>
2015-11-11 23:06:19 -0500

    Constructing structs for seasons/shows/episodes

--- main.go
+++ main.go
@@ -18,35 +18,66 @@ var (
 	searchURL      = hostURL + "/search?from=&q="
 )
 
+type Show struct {
+	Title   string
+	Path    string
+	Url     string
+	Seasons []Season
+}
+
+type Season struct {
+	Title    string
+	Number   int
+	Length   int
+	Episodes []Episode
+}
+
+type Episode struct {
+	Title  string
+	Number int
+	Url    string
+}
+
 func main() {
+	for {
+		//TODO Form a VPN connection and output the IP address what will be used for the connection
 
-	//TODO Form a VPN connection and output the IP address what will be used for the connection
+		//TODO Request the users login credentials and build a cookie for required auth requests
 
-	//TODO Request the users login credentials so
+		// First we as the user for what show they would like to rip
+		getStandardUserInput("Please enter a show name : ", &showSearchTerm)
 
-	// First we as the user for what show they would like to rip
-	getStandardUserInput("Please enter a show name : ", &showSearchTerm)
+		// First we get the showname/path of the show we would like to download
+		show, err := searchShowPath(showSearchTerm)
+		if err != nil || show.Url == "" {
+			fmt.Println(">>> Unable to get a show name/path via search results. \n", err)
+			continue
+		}
+		fmt.Println("\nDetermined a valid show name of : --- " + show.Title + " ---")
 
-	// First we get the showname/path of the show we would like to download
-	showPath, err := searchShowPath(showSearchTerm)
-	if err != nil || showPath == "" {
-		fmt.Println("Unable to get a show name/path via search results. ", err)
-		return
+		// Asks the user if the title of the series recieved is correct before proceeding...
+		input := ""
+		getStandardUserInput("Is this correct? : ", &input)
+		if input == "N" || input == "n" || input == "no" || input == "NO" || input == "nO" {
+			fmt.Println(">>> Sorry about that! We're not fucking perfect! \n")
+			continue
+		}
+
+		// Gets the episodes for the show recieved
+		show, err = getEpisodes(show)
+		if err != nil || len(show.Seasons) == 0 || len(show.Seasons[0].Episodes) == 0 {
+			fmt.Println(">>> Unable to get any episodes for the show specified. \n", err)
+			continue
+		}
+
+		// Attempts to access and print the titles of all seasons recieved
+		fmt.Println("\nBelow is a list of all seasons found ...\n")
+		for i := 0; i < len(show.Seasons); i++ {
+			fmt.Println("	- " + show.Seasons[i].Title + " (" + strconv.Itoa(len(show.Seasons[i].Episodes)) + " Episodes)")
+		}
+
+		//TODO RTMP Dumps each episode in a seperate goroutine...
 	}
-	fmt.Println("Determined a valid show name of : --- " + strings.Title(strings.Replace(showPath, "-", " ", -1)) + " ---")
-
-	// Next we attempt to get an array of episode URLS via the results page
-	showURL := hostURL + "/" + showPath
-
-	// Gets a 2-dimentional array of episode URLS
-	episodeList, err := getEpisodeList(showURL)
-	if err != nil || len(episodeList) == 0 {
-		fmt.Println("Unable to get any episode URLS. ", err)
-		return
-	}
-	fmt.Println("Recieved the following list of episodes : ", episodeList)
-
-	//TODO RTMP Dumps each episode in a seperate goroutine...
 }
 
 // Gets user input from the user and unmarshalls it into the input
@@ -65,42 +96,63 @@ func getStandardUserInput(prefixText string, input *string) error {
 
 // Takes the passed show name and searches crunchyroll,
 // taking the first showname found as the show
-func searchShowPath(showName string) (string, error) {
+func searchShowPath(showName string) (Show, error) {
+
 	// Reforms showName string to url param
 	encodedShowName := strings.ToLower(strings.Replace(showName, " ", "+", -1))
 
 	// Attempts to grab the contents of the search URL
-	fmt.Println("Attempting to determine the URL for passed show : " + showName)
+	fmt.Println("\nAttempting to determine the URL for passed show : " + showName)
 	fmt.Println(">> Accessing search URL : " + searchURL + encodedShowName)
 	resp, err := http.Get(searchURL + encodedShowName)
 	if err != nil {
-		return "", err
+		return Show{}, err
 	}
 	defer resp.Body.Close()
 
 	// Breaks HTML string into a reader that we can search for HTML element nodes
 	searchBytes, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return "", err
+		return Show{}, err
 	}
 	doc, err := html.Parse(strings.NewReader(string(searchBytes)))
 	if err != nil {
-		return "", err
+		return Show{}, err
 	}
 	var findFirstShow func(*html.Node)
 
 	// Handles searching for the url of the show by the first <a href="http://www.crunchyroll.com..."
-	resultsArray := []string{}
+	URLArray := []string{}
+	seriesTitle := ""
+	titleFlag := false
+
 	findFirstShow = func(n *html.Node) {
 		if n.Type == html.ElementNode && n.Data == "a" {
 			for _, a := range n.Attr {
 				if a.Key == "href" {
 					if strings.Contains(a.Val, hostURL) {
-						resultsArray = append(resultsArray, a.Val)
+						URLArray = append(URLArray, a.Val)
 						break
 					}
 				}
 			}
+		}
+		// Activator for title reading after span class="series"
+		if n.Type == html.ElementNode && n.Data == "span" {
+			for _, span := range n.Attr {
+				if span.Key == "class" && span.Val == "series" {
+					titleFlag = true // power up the bass cannon
+					break
+				}
+				if span.Key == "class" && span.Val == "name" {
+					titleFlag = false // power down the bass cannon
+					break
+				}
+			}
+		}
+		// Wait for titleFlag to be tripped by class="series" before setting series title
+		if n.Type == html.TextNode && n.Data != "" && titleFlag && seriesTitle == "" {
+			seriesTitle = n.Data
 		}
 		for c := n.FirstChild; c != nil; c = c.NextSibling {
 			findFirstShow(c)
@@ -108,47 +160,58 @@ func searchShowPath(showName string) (string, error) {
 	}
 	findFirstShow(doc)
 
-	// Gets the first result from our parse search and returns the path if its not ""/store/""
-	firstPath := strings.Replace(resultsArray[0], hostURL+"/", "", 1)
+	// Gets the first result from our parse search and returns the path if its not ""/store/" or "/crunchygay/"
+	firstPath := strings.Replace(URLArray[0], hostURL+"/", "", 1)
 	firstShowPath := strings.Split(firstPath, "/")[0]               // Gets only the first path name (ideally a show name)
 	if firstShowPath == "store" || firstShowPath == "crunchycast" { // tf is a crunchycast?
-		return "", nil
+		return Show{}, nil
 	}
-	return firstShowPath, nil
+
+	// Packs up all assumed show information and returns it
+	return Show{
+		Title: seriesTitle,   // Series name recieved from javascript json
+		Path:  firstShowPath, // Show path retrieved from a href url
+		Url:   hostURL + "/" + firstShowPath,
+	}, nil
 }
 
 // Given a show name, returns an array of urls of
 // all the episodes associated with that show
-func getEpisodeList(showURL string) ([]string, error) {
+func getEpisodes(show Show) (Show, error) {
 	// Attempts to grab the contents of the show page
-	fmt.Println(">> Accessing show page URL : " + showURL)
-	resp, err := http.Get(showURL)
+	fmt.Println("\n>> Accessing show page URL : " + show.Url)
+	resp, err := http.Get(show.Url)
 	if err != nil {
-		return []string{}, err
+		return show, err
 	}
 	defer resp.Body.Close()
 
 	// Parses the HTML in order to search for the list of episodes
 	showBytes, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return []string{}, err
+		return show, err
 	}
 	doc, err := html.Parse(strings.NewReader(string(showBytes)))
 	if err != nil {
-		return []string{}, err
+		return show, err
 	}
 	var findEpisodes func(*html.Node)
 
+	seasonArray := []Season{Season{}}
+	episodeArray := []Episode{}
+
 	// Handles searching for the url of the show by the first <a href="http://www.crunchyroll.com..."
-	episodeArray := []string{}
-	episodeTotal := 0
 	findEpisodes = func(n *html.Node) {
 		// If we see an li that resembles a new season list
 		if n.Type == html.ElementNode && n.Data == "li" {
 			for _, li := range n.Attr {
-				if li.Key == "class" && strings.Contains(li.Val, "season") {
-					// Appends a "SEASONBREAK" string so we can separate seasons in the array
-					episodeArray = append(episodeArray, "SEASONBREAK")
+				if li.Key == "class" && strings.Contains(li.Val, "season ") {
+					// Appends a new season to the seasonArray which we will add episodes to
+					seasonArray[len(seasonArray)-1].Episodes = episodeArray
+
+					// Clears out the episodeArray and sets up a new empty season for populating the new season
+					seasonArray = append(seasonArray, Season{})
+					episodeArray = []Episode{}
 					break
 				}
 			}
@@ -156,9 +219,37 @@ func getEpisodeList(showURL string) ([]string, error) {
 		// If we see an 'a' that represents a new episode
 		if n.Type == html.ElementNode && n.Data == "a" {
 			for _, a := range n.Attr {
+				if a.Key == "title" && seasonArray[len(seasonArray)-1].Title == "" {
+					// Stores the series title so we can use it on the season title
+					seasonArray[len(seasonArray)-1].Title = a.Val
+				}
 				if a.Key == "href" && strings.Contains(a.Val, "/episode-") {
-					episodeArray = append(episodeArray, hostURL+a.Val)
-					episodeTotal = episodeTotal + 1
+					// Pulls all the episode information from the URL in order to populate a new episode object
+					episodePathArray := strings.Split(strings.Split(a.Val, "/")[2], "-")
+
+					// Gets episode name from a.Val
+					episodeName := ""
+					for i := 2; i < len(episodePathArray)-1; i++ {
+						if i != 2 {
+							episodeName = episodeName + " "
+						}
+						episodeName = episodeName + episodePathArray[i]
+					}
+					episodeName = strings.Title(episodeName)
+
+					// Gets episode number from a.Val
+					episodeNumber, err := strconv.Atoi(episodePathArray[1])
+					if err != nil {
+						episodeNumber = 0
+					}
+
+					// Gets episode URL from a.Val
+					episodeURL := hostURL + a.Val
+					episodeArray = append(episodeArray, Episode{
+						Title:  episodeName,
+						Number: episodeNumber,
+						Url:    episodeURL,
+					})
 					break
 				}
 			}
@@ -169,16 +260,31 @@ func getEpisodeList(showURL string) ([]string, error) {
 	}
 	findEpisodes(doc)
 
-	// Re-arranges array so that first episode is first instead of last
-	finalEpisodeArray := []string{}
-	for i := len(episodeArray) - 1; i >= 0; i-- {
-		finalEpisodeArray = append(finalEpisodeArray, episodeArray[i])
+	// Adds the final episode array (in most cases season 1) to the seasons array
+	seasonArray[len(seasonArray)-1].Episodes = episodeArray
+
+	// Re-arranges seasons array so that first season is first instead of last
+	finalSeasonArray := []Season{}
+	for i := len(seasonArray) - 1; i >= 0; i-- {
+		if len(seasonArray[i].Episodes) > 0 {
+			finalSeasonArray = append(finalSeasonArray, seasonArray[i])
+		}
 	}
 
-	// TODO Handle season breaks and organize episodes into objects
+	// Re-arranges episode in each season so we have first to last
+	for i := 0; i < len(finalSeasonArray); i++ {
+		finalEpisodeArray := []Episode{}
+		for n := len(finalSeasonArray[i].Episodes) - 1; n >= 0; n-- {
+			finalEpisodeArray = append(finalEpisodeArray, finalSeasonArray[i].Episodes[n])
+		}
+		finalSeasonArray[i].Episodes = finalEpisodeArray
+		finalSeasonArray[i].Length = len(finalEpisodeArray)
+	}
+
 	// TODO Filter out episodes that aren't yet released (ex One Piece)
 
 	// Prints and returns the array of episodes that we we're able to get
-	fmt.Println("Returning a total of " + strconv.Itoa(episodeTotal) + " episodes.")
-	return finalEpisodeArray, nil
+	show.Seasons = finalSeasonArray
+	fmt.Println("Recieved a total of " + strconv.Itoa(len(show.Seasons)) + " seasons...")
+	return show, nil
 }

c025de26f63d34e1bba9f37d63e5d70cb15342b1
Steven <steven@swolfe.me>
2015-11-28 16:49:07 -0500

    Performing some standard input in main to get user info

--- main.go
+++ main.go
@@ -6,28 +6,73 @@ import "fmt"
 
 func main() {
 	session := new(Session)
-	_ = session.Login("", "")
+	// Intro header
+	fmt.Printf("------------------------------------------------------------\n")
+	fmt.Printf("------------------ CrunchyRip 1.0 by Viz_ ------------------\n")
+	fmt.Printf("------------------------------------------------------------\n")
+
+	username := ""
+	password := ""
+	fmt.Printf("Please login to Crunchyroll below...\n")
+	getStandardUserInput("Username : ", &username)
+	getStandardUserInput("Password : ", &password)
+
+	err := session.Login(username, password)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	show := new(Show)
-	show.FindShow("tempest", session.Cookies)
+
+	desiredShow := ""
+	getStandardUserInput("Enter a Show name : ", &desiredShow)
+	show.FindShow(desiredShow, session.Cookies)
+
+	// Gets all episodes associated with the show found
 	show.GetEpisodes(session.Cookies)
-	// Creates new show folder
+	// TODO Create new show folder
 	for _, season := range show.Seasons {
-		// Creates new season folder
+		// TODO Create new season folder
 		for _, episode := range season.Episodes {
 			fmt.Printf(episode.FileName + "\n")
+
 			fmt.Printf("Downloading video...\n")
-			episode.DownloadEpisode("1080p", session.Cookies)
+			err = episode.DownloadEpisode("1080p", session.Cookies)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
 			fmt.Printf("Downloading subtitles...\n")
-			episode.DownloadSubtitles("English", session.Cookies)
+			err = episode.DownloadSubtitles("English", session.Cookies)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
 			fmt.Printf("Splitting video...\n")
-			Split(episode.FileName)
+			err = Split(episode.FileName)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
 			fmt.Printf("Merging video...\n")
-			Merge(episode.FileName)
+			err = Merge(episode.FileName)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
 			fmt.Printf("Cleaning video...\n")
-			Clean(episode.FileName)
+			err = Clean(episode.FileName)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
 			fmt.Printf("Downloading and merging completed successfully.\n\n")
-			// Moves show
+			// TODO Move shows
 		}
 	}
 }

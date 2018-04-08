565601ad5860f288fe93313159e8a3054478020d
theclickman <38191912+theclickman@users.noreply.github.com>
2018-04-08 16:28:15 -0400

    Update to GetTitle()
    Update to GetTitle() to use CleanFileName and remove illegal characters to show title, otherwise it fail to create folder properly.

--- crunchyroll/show.go
+++ crunchyroll/show.go
@@ -97,7 +97,8 @@ func (s *Show) Scrape(client *anirip.HTTPClient, showURL string) error {
 
 // Gets the title of the show for referencing outside of this lib
 func (show *Show) GetTitle() string {
-	return show.Title
+	//CleanFileName to remove illegal chars from show name, if any.
+	return anirip.CleanFilename(show.Title)
 }
 
 // Re-stores seasons belonging to the show and returns them for iteration
